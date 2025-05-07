0933b47fa1ea82c6c866433a8ab654a5fa1df40e
HenrySpark369 <henryfordjunior@hotmail.com>
2025-05-06 22:43:50 -0600

    feat: registrar blueprint de configuración en la inicialización de la app

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,13 @@ def create_app():
     from .routes.metrics import metrics_bp
     from .routes import cruces
     from .routes import ordenes
+    from .routes import config
+
     app.register_blueprint(dashboard.bp)
     app.register_blueprint(backtest.bp)
     app.register_blueprint(live.bp)
     app.register_blueprint(metrics_bp)
     app.register_blueprint(ordenes.bp_ordenes)
     app.register_blueprint(cruces.cruces_bp)
+    app.register_blueprint(config.config_bp)
     return app
